74ae2a6f3adcab1fdec3fcfaff62503a49ca216f
Lxy <2849674510@qq.com>
2024-04-12 23:43:39 +0800

    zz

--- src/usercomponents/UserActivityCard.vue
+++ src/usercomponents/UserActivityCard.vue
@@ -1,3 +1,29 @@
 <template>
-    1212
+  <a-list-item class="list-demo-item" action-layout="vertical">
+    <template #actions>
+      <div class="layout-demo">
+        <a-layout style="height: 40px;">
+          <a-layout-header> <span><icon-clock-circle />&nbsp;开始时间：2024.04.08</span>
+            <span><icon-clock-circle />&nbsp;结束时间：2024.04.09</span></a-layout-header>
+          <a-layout-content><span><icon-user-group />参与人数</span>
+            <span><icon-location />活动位置</span>
+            <a-link href="link">查看详情</a-link></a-layout-content>
+
+        </a-layout>
+
+
+      </div>
+    </template>
+
+    <a-list-item-meta title="活动标题" description="v关于军方击毙iui哦i和口语胡一负于覅u关于工艺i覅与i故意犯规与开发有">
+
+
+      <template #title>
+        <a-typography-text strong>大师的dffgsfsd</a-typography-text>
+
+      </template>
+
+    </a-list-item-meta>
+  </a-list-item>
 </template>
+<!--  -->
--- src/views/UserActivicy.vue
+++ src/views/UserActivicy.vue
@@ -1,35 +1,33 @@
 <template>
-    <a-col :offset="3" :span="20">
-        <a-row>
-            <a-col :span="20">
-                <a-page-header title="活动中心" @back="() => {
-                    router.back();
-                }">
-                    <template #extra>
-                        <div style="height: 100%; display: flex; justify-content: flex-end;">
-                            <el-button type="primary" size="large" @click="gotoMyActivity">我的活动</el-button>
-                        </div>
+  <a-col :offset="3" :span="20">
+    <a-row>
+      <a-page-header title="近期活动" subtitle="活动中心" @back="() => {
+        router.back();
+      }">
+      <template #extra>
+        <a-space></a-space>
+      <a-link href="link" status="warning"><icon-message />我的活动</a-link>
                     </template>
-                </a-page-header>
-            </a-col>
+      </a-page-header>
+    </a-row>
+    <br>
+    <a-row>
+      <a-col :offset="3" :span="16">
+        <a-row v-for="i in 10" :key="i">
+          <UserActivityCard></UserActivityCard>
+          <a-divider></a-divider>
         </a-row>
         <br>
-        <a-row>
-            <a-col :offset="1">
-                <h2>近期活动</h2>
-            </a-col>
-        </a-row>
-        <a-row>
-            <a-col :offset="3" :span="15">
-                <a-row v-for="i in 10" :key="i">
-                    <UserActivityCard :mode="true"></UserActivityCard>
-                    <a-divider></a-divider>
-                </a-row>
-                <br>
-            </a-col>
-        </a-row>
+        <el-row>
+          <el-col :span="10" :offset="8">
+            <el-pagination :page-size="10" background layout="prev, pager, next, jumper" :total="1000" />
+          </el-col>
+        </el-row>
         <br>
-    </a-col>
+      </a-col>
+    </a-row>
+    <br>
+  </a-col>
 </template>
 
 <script lang="ts" setup>
@@ -38,10 +36,4 @@ import { useRouter } from "vue-router";
 
 const router = useRouter();
 
-const gotoMyActivity = () => {
-    router.push("/user/myactivity");
-};
-
 </script>
-
-<style scoped></style>
